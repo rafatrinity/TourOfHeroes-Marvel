510402ce9702724222c480127607e9432448cfa4
Rafael Trindade <rafaeltrindade649@gmail.com>
2019-02-14 13:10:01 -0200

    gerando core

---
+++ TourOfHeroes-Marvel/src/app/personagens.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClientModule }    from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonagensService {
+
+  constructor() { }
+}
