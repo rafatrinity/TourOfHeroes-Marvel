e3fc14f10f5cd732362100a62ec6b41cf87ec7bc
Rafael Trindade <rafaeltrindade649@gmail.com>
2019-03-02 16:45:26 -0300

    retirando pasta stack

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ var timestamp = Number(new Date());
 var hash = Md5.hashStr(timestamp + "49edd2b6b36131f85cd978fc4aca2da7ea18675874980acd1fc05c9b9f9316de92dc33b7");
 export const environment = {
 	production: true,
-  apiUrl: `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&orderBy=-modified&limit=10&apikey=74980acd1fc05c9b9f9316de92dc33b7&hash=${hash}`,
+	apiUrl: `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&orderBy=-modified&limit=12&apikey=74980acd1fc05c9b9f9316de92dc33b7&hash=${hash}`,
 	settings: {
 		appErrorPrefix: '[Angular Heroes Error] ',
 		appTitle: 'Angular Heroes',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,13 +8,13 @@ let md5 = new Md5();
 var timestamp = Number(new Date());
 var hash = Md5.hashStr(timestamp + "49edd2b6b36131f85cd978fc4aca2da7ea18675874980acd1fc05c9b9f9316de92dc33b7");
 export const environment = {
-  production: true,
-  apiUrl: `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&orderBy=-modified&limit=10&apikey=74980acd1fc05c9b9f9316de92dc33b7&hash=${hash}`,
-  settings: {
-    appErrorPrefix: '[Angular Heroes Error] ',
-    appTitle: 'Angular Heroes',
-    version: '1.0.0'
-  }
+	production: true,
+	apiUrl: `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&orderBy=-modified&limit=12&apikey=74980acd1fc05c9b9f9316de92dc33b7&hash=${hash}`,
+	settings: {
+		appErrorPrefix: '[Angular Heroes Error] ',
+		appTitle: 'Angular Heroes',
+		version: '1.0.0'
+	}
 };
 
 /*
