4fdfaf3c8fd9bc0b9c6aafdfc984eb43a442e5e0
Rafael Trindade <rafaeltrindade649@gmail.com>
2019-03-03 16:25:38 -0300

    heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+// Replace the '/dist/marvel-heroes123'
+app.use(express.static(__dirname + '/dist/marvel-heroes123'));
+
+app.get('*', function(req,res) {
+  // Replace the '/dist/marvel-heroes123/index.html'
+  res.sendFile(path.join(__dirname + '/dist/marvel-heroes123/index.html'));
+});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
