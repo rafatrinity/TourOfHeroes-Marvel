9cc9df8d4c8bfe02d9b3a525e9baea93b08ed04d
Rafael Trindade <rafaeltrindade649@gmail.com>
2019-03-05 00:13:06 -0300

    atualizando

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // Replace the '/dist/marvel-heroes123'
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/marvel-heroes123'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req,res) {
   // Replace the '/dist/marvel-heroes123/index.html'
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/marvel-heroes123/index.html'));
 });
