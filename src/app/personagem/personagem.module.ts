8179009abca59a974332fff0d234b1e7a5786bb4
Rafael Trindade <rafaeltrindade649@gmail.com>
2019-06-19 11:36:22 -0300

    voltando

--- src/app/personagem/personagem.module.ts
+++ src/app/personagem/personagem.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PersonagemService } from '../services/personagem.service';
-
+import { BlockUi } from 'ngx-ui-hero';
 @NgModule({
 	imports: [
 	CommonModule,
